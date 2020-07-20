83e7d3d749ca5352d13baca6f2e32daf3029d38c
AlexanderPilot <alexander.pilot@gmx.de>
2020-07-20 14:39:45 +0200

    Initial Commit PlatformIO

---
+++ TimetrackerCube/src/main.cpp
@@ -0,0 +1,43 @@
+//Einbinden von Bibliotheken
+#include <Arduino.h>
+
+//Definition globale Variablen
+
+
+//Definition Objekte
+
+
+//Definition Funktionen
+
+void setup() {
+    //Initialisiere Gyro-Sensor
+
+    //Initialisiere OLED
+
+    //Initialisierung RTC
+
+    //Initialisiere Batterie-Monitoring
+
+    //Starte WiFi
+
+    //Initialisiere Webserver
+
+    //Initialisiere Datenbank
+
+    //Lesen EEPROM Daten
+
+}
+
+void loop() {
+    //Definition von Variabelen
+    
+    //Wakeup aus Deep Sleep
+
+    //Prüfe Würfel-Ausrichtung
+        //Stellung 0 --> gehe in Deep Sleep
+        //Stellung 1-6 --> Anwendungen
+            //Zählen der Dauer der Würfelstellung
+
+    //Speichern der Daten in EEPROM
+
+}
