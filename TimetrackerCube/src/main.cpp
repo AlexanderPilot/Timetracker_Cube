c88288c6731085461b5633e14765c055e30d685d
Alexander Pilot <62375738+AlexanderPilot@users.noreply.github.com>
2021-08-09 11:00:37 +0200

    Update main.cpp

--- TimetrackerCube/src/main.cpp
+++ TimetrackerCube/src/main.cpp
@@ -1,16 +1,28 @@
 //Einbinden von Bibliotheken
 #include <Arduino.h>
+#include "GY521.h" //Library for Gyro-Sensor
+#include "RTClib.h"
+
 
 //Definition globale Variablen
 
 
 //Definition Objekte
-
+GY521 gyro(0x69);
 
 //Definition Funktionen
 
-void setup() {
+void setup()
+{
+    //Initialisiere I2C
+    Wire.begin();
     //Initialisiere Gyro-Sensor
+    while (sensor.wakeup() == false)
+    {
+        Serial.print(millis());
+        Serial.println("\tCould not connect to GY521");
+        delay(1000);
+    }
 
     //Initialisiere OLED
 
@@ -19,6 +31,9 @@ void setup() {
     //Initialisiere Batterie-Monitoring
 
     //Starte WiFi
+    SSID = "SSID";
+    PW = "PW";
+    WiFi.begin(SSID. PW);
 
     //Initialisiere Webserver
 
@@ -34,6 +49,10 @@ void loop() {
     //Wakeup aus Deep Sleep
 
     //Prüfe Würfel-Ausrichtung
+    sensor.read();
+    float pitch = sensor.getPitch();
+    float roll  = sensor.getRoll();
+    float yaw   = sensor.getYaw();
         //Stellung 0 --> gehe in Deep Sleep
         //Stellung 1-6 --> Anwendungen
             //Zählen der Dauer der Würfelstellung
